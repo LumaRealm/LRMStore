11b143be79ae5e2638dbb908b85549cbf7fc5e84
LumaRealm <noamgrayson@gmail.com>
2024-11-29 11:29:10 +0500

    first commit

---
+++ inertia/pages/home.tsx
@@ -0,0 +1,33 @@
+import { Head } from '@inertiajs/react'
+import "~/css/SomeName.scss"
+import MyImg from '~/images/lrm2.webp'
+
+export default function Home() {
+  return (
+    <>
+      <Head title="Homepage" />
+
+
+    <div className="splash">
+      <div className="splash_logo">
+        <img src={MyImg} alt="" />
+      </div>
+      <div className="splash_svg">
+        <svg width="100%" height="100%">
+          <rect width="100%" height="100%" />
+        </svg>
+      </div>
+      <div className="splash_minimize">
+        <svg width="100%" height="100%">
+          <rect width="100%" height="100%" />
+        </svg>
+      </div>
+    </div>
+    <div className="text">
+      <p>Duis quis</p>
+      <p>nec sapien</p>
+      <button>More</button>
+    </div>
+    </>
+  )
+}
